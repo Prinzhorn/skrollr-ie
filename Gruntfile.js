be30672c5afeff59d76b4a6e329ce3927a53707a
Alexander Prinzhorn <alex@nder-prinzhorn.de>
2013-05-18 14:27:05 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+	//Configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json') ,
+		jshint: {
+			options: {
+				smarttabs: false,
+				curly: true,
+				immed: true,
+				latedef: true,
+				noarg: true,
+				quotmark: 'single',
+				undef: true,
+				unused: true,
+				strict: true,
+				trailing: true,
+				globals: {
+					window: true,
+					document: true,
+					navigator: true
+				}
+			},
+			all: ['src/**/*.js']
+		},
+		uglify: {
+			options: {
+				banner: '/*! skrollr-ie <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>) | Alexander Prinzhorn - https://github.com/Prinzhorn/skrollr | Free to use under terms of MIT license */\n'
+			},
+
+			all: {
+				files: {
+					'dist/skrollr.ie.min.js': 'src/skrollr.ie.js'
+				}
+			}
+		}
+	});
+
+	//Dependencies.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	//Tasks.
+	grunt.registerTask('default', ['jshint', 'uglify']);
+};
--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 skrollr-ie
 ==========
 
-skrollr plugin that adds some missing features to old IE
+skrollr plugin that adds some missing features to IE < 9
+
+The plugin makes IE understand `opacity`, `rgb()` and `hsl()` (the ones with alpha are mapped to them) and it creates a very simple `document.querySelector` polyfill which only supports ID selectors (using `getElementById`). Needed if you want to use [data-anchor-target](https://github.com/Prinzhorn/skrollr#relative-mode-or-viewport-mode).
+
+Documentation
+=====
+
+Include `dist/skrollr.ie.min.js` after the core using conditional comments.
+
+```html
+<script type="text/javascript" src="skrollr.min.js"></script>
+
+<!--[if lt IE 9]>
+<script type="text/javascript" src="skrollr.ie.min.js"></script>
+<![endif]-->
+```
+
+
+Changelog
+====
+
+0.1.0 (2013-05-18)
+-----
+
+* Moved skrollr-ie to a dedicated repo.
